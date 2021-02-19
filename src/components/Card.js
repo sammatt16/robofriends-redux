396189807ab6cd78449e44c15b0caaab7fbffef8
unknown <samatth16@gmail.com>
2021-02-19 13:26:58 -0500

    new repo

--- src/components/Card.js
+++ src/components/Card.js
@@ -1,15 +1,15 @@
 import React from 'react';
 
-const Card = ({ name, email, id }) => {
-  return (
-    <div className='tc grow bg-light-green br3 pa3 ma2 dib bw2 shadow-5'>
-      <img alt='robots' src={`https://robohash.org/${id}?200x200`} />
-      <div>
-        <h2>{name}</h2>
-        <p>{email}</p>
-      </div>
+const Card = ({name, email, id}) =>{
+   return(
+    <div className='tc bg-light-green dib bre pa3 ma2 grow'>
+        <img alt='robots' src={`https://robohash.org/${id}?200x200`}/>
+        <div>
+            <h2>{name}</h2>
+            <p>{email}</p>
+        </div>
     </div>
-  );
+    ); 
 }
 
 export default Card;
